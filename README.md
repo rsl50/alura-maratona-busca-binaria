856c9ee695f35068b56a6f91769a35d9cf2394d9
Robson Lima <rsl.robson@gmail.com>
2020-01-15 11:10:55 -0300

    Commit para o desafio usando algoritmo de busca binária

---
+++ README.md
@@ -0,0 +1,56 @@
+Curso de Maratona de programação: Algoritmos para sua primeira competição
+---------
+<img src="https://www.alura.com.br/assets/api/cursos/maratona-de-programacao.svg" data-canonical-src="https://www.alura.com.br/assets/api/cursos/maratona-de-programacao.svg" width="250" height="250" />
+
+https://cursos.alura.com.br/course/maratona-de-programacao
+
+## Temas abordados
+* Começando o treinamento
+* Ordenação
+* Recursão
+* Busca binária
+* Introdução à Programação dinâmica
+
+
+## Binary search
+BSEARCH1 - Binary search
+
+https://www.spoj.com/problems/BSEARCH1/
+
+You are given a sorted array of numbers, and followed by number of queries, for each query if the queried number is present in the array print its position, else print -1.
+
+
+**Input**
+
+First line contains N Q, number of elements in the array and number of queries to follow,
+
+Second line contains N numbers, elements of the array, each number will be:
+> -10^9 ≤ ai ≤ 10^9
+
+> 0 < N ≤ 10^5
+
+> 0 < Q ≤ 5*10^5
+50
+
+**Output**
+
+For each element in the query, print the elements 0 based location of its first occurence, if present, otherwise print -1.
+
+
+**Examples**
+
+```
+Input:
+5 4
+2 4 7 7 9
+7
+10
+4
+2
+
+Output:
+2
+-1
+1
+0
+````
---
+++ binsearch.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ==================================================
+// ==================================================
+// https://www.ime.usp.br/~pf/algoritmos-livro/slides/SLIDES-A.pdf
+int BuscaBinaria2 (int x, int n, int v[]) {
+	return BuscaBinR (x, -1, n, v);
+}
+
+int BuscaBinR (int x, int e, int d, int v[]) {
+	if (e == d-1) return d;
+	else {
+		int m = (e + d)/2;
+		if (v[m] < x)
+			return BuscaBinR (x, m, d, v);
+		else
+			return BuscaBinR (x, e, m, v);
+	}
+}
+// ==================================================
+// ==================================================
+int BuscaBinaria (int x, int n, int v[]) {
+	int e, m, d;
+	e = -1; d = n;
+	while (/*X*/ e < d - 1) {
+		m = (e + d)/2;
+		if (v[m] < x) e = m;
+		else d = m;
+	}
+	return d;
+}
+// ==================================================
+// ==================================================
+
+int busca_binaria (int inicio, int fim, int valor, int array[]) {
+	if (inicio > fim) return -1;
+	
+	if (inicio == fim) {
+		if (array[inicio] == valor) return inicio;
+		else return -1;
+	} 
+	
+	int meio = (inicio + fim) / 2;
+	
+	if (array[meio] < valor) {
+		return busca_binaria(meio + 1, fim, valor, array);
+	} else if (array[meio] == valor) {
+		return busca_binaria(inicio, meio, valor, array);
+	} else {
+		return busca_binaria(inicio, meio - 1, valor, array);
+	}
+}
+
+
+int main () {
+
+	FILE * fp;
+	fp = freopen("input1.txt", "r", stdin);
+
+	char buf[17];
+	memset(buf, 0, 17);
+
+	if (fp != NULL) {
+		while(fgets(buf, 17, fp) != NULL) {
+			
+			long int n, q;
+			
+			//fgets(buf, 17, fp);
+			sscanf(buf, "%d %d", &n, &q);
+			//printf("%d %d\n", n, q);
+			
+			int array[n];
+			int i;
+			char aux[n * 13 + 2];
+			char *pEnd;
+			memset(aux, 0, n * 13 + 2);			
+			
+			fgets(aux, n * 13 + 2, fp);
+			for (i = 0; i < n; i++) {
+				array[i] = strtof(aux, &pEnd);
+				memcpy(aux, &pEnd[1], strlen(pEnd));
+				//printf("%d ", array[i]);
+			}
+			
+			for (i = 0; i < q; i++) {
+				fgets(buf, 17, fp);
+				int val = strtol(buf, NULL, 10);
+				//printf("%d\n", val);
+				
+				printf("%d\n", busca_binaria(0, n - 1, val, array));
+			}
+		}
+	}
+	
+	return (0);
+}
